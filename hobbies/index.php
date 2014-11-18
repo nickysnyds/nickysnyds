e0e145cae4e85ae3774da51b75d5d801bf8c2785
nickysnyds <nicksnyder19@gmail.com>
2014-11-18 14:34:59 -0800

    nickysnyds.com
    All content for nickysnyds.com as of 11/18/14

---
+++ hobbies/index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/css/main.css">
+    <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,700,600' rel='stylesheet' type='text/css'>
+    <link rel="shortcut icon" href="/images/Nickcircle.png">
+  </head>
+
+  <body>
+    <div class="nav">
+    	<ul class="pull-left">
+    	  <li><a href="../">Home</a></li>
+    	  <li><a href="../experience">Experience</a></li>
+    	  <li><a href="../education">Education</a></li>
+    	</ul>
+    	<ul class="pull-right">
+    	  <li><a href="../hobbies">Hobbies</a></li>
+    	  <li><a href="../references">References</a></li>
+    	  <li><a href="../contact">Contact</a></li>
+    	</ul>
+    </div>
+
+    <div class="jumbotron" id="hockey">
+    </div>
+
+    <div class="jumbotron" id="surfing">
+    </div>
+
+    <div class="jumbotron" id="snow">
+    </div>
+
+    <div class="jumbotron" id="guitar">
+	</div>
+
+	<section class="footer-links"> 
+		<!-- FOOTER MENU -->
+		<ul class="footer-menu">
+		  <li class="subfooter">
+		    <a href="/experience" class="title">Experience</a>
+		    <ul>
+		      <li class="items"><a href="http://www.invoca.com" target="_blank">Invoca</a></li>
+		      <li class="items"><a href="http://www.sproutup.org" target="_blank">Sprout Up</a></li>
+		      <li class="items"><a href="http://gauchosplay.com/" target="_blank">UCSB Recreation Sports</a></li>
+		    </ul>
+		  </li> 
+		  <li class="subfooter">
+		    <a href="/education" class="title">Education</a>
+		    <ul>
+		      <li class="items"><a href="http://www.ucsb.edu/" target="_blank">University of California, Santa Barbara</a></li>
+		      <li class="items"><a href="http://www.es.ucsb.edu/" target="_blank">Environmental Studies</a></li>
+		      <li class="items"><a href="http://www.tmp.ucsb.edu/" target="_blank">Technology Management Program</a></li>
+		    </ul>
+		  </li>
+		  <li class="subfooter">
+		    <a href="/hobbies" class="title">Hobbies</a>
+		    <ul></ul>
+		  </li>
+		  <li class="subfooter">
+		    <a href="/references" class="title">References</a>
+		    <ul></ul>
+		  </li>
+		  <li class="subfooter">
+		    <a href="/contact" class="title">Contact</a>
+		      <ul>
+		        <li class="items"><a href="mailto:info@nickysnyds.com">Email</a></li>
+		        <li class="items"><a href="tel:855-977-7567">Phone</a></li>
+		      </ul>
+		  </li>
+		</ul>
+		<!-- END/ FOOTER MENU --> 
+		<div class="footer-social">
+		  <ul class="footer-social-nav">
+		    <li class="facebook"><a href="https://www.facebook.com/nicholastsnyder" title="Facebook" target="_blank"><i class="fa fa-facebook fa-lg"></i></a></li>
+		    <li class="twitter"><a href="https://twitter.com/nickysnyds" title="Twitter" target="_blank"><i class="fa fa-twitter fa-lg"></i></a></li>
+		    <li class="google"><a href="https://plus.google.com/118033681540619304202/posts" title="Google+" target="_blank"><i class="fa fa-google-plus fa-lg"></i></a></li>
+		    <li class="linkedin"><a href="http://www.linkedin.com/in/nickysnyds" title="LinkedIn" target="_blank"><i class="fa fa-linkedin fa-lg"></i></a></li>
+		  </ul>
+		</div>
+	 </section>
+	</body>
+</html>
+
+
+
+
---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/css/main.css">
+    <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,700,600' rel='stylesheet' type='text/css'>
+    <link rel="shortcut icon" href="/images/Nickcircle.png">
+  </head>
+
+  <body>
+    <div class="nav">
+      <ul class="pull-left">
+        <li><a href="#">Home</a></li>
+        <li><a href="experience">Experience</a></li>
+        <li><a href="education">Education</a></li>
+      </ul>
+      <ul class="pull-right">
+        <li><a href="hobbies">Hobbies</a></li>
+        <li><a href="references">References</a></li>
+        <li><a href="contact">Contact</a></li>
+      </ul>
+    </div>
+
+    <div class="jumbotron" id="homepage">
+    </div>
+    <div id="header">
+      <h1>Nick Snyder</h1>
+        <p>Entrepreneur</p>
+      <div id="contactbody">
+        <ul class="social-nav">
+          <li class="email"><a href="mailto:info@nickysnyds.com" title="Email"><i class="fa fa-envelope fa-lg"></i></a></li>
+          <li class="phone"><a href="tel:855-977-7567" title="Phone"><i class="fa fa-phone fa-lg"></i></a></li>
+          <li class="facebook"><a href="https://www.facebook.com/nicholastsnyder" title="Facebook" target="_blank"><i class="fa fa-facebook fa-lg"></i></a></li>
+          <li class="twitter"><a href="https://twitter.com/nickysnyds" title="Twitter" target="_blank"><i class="fa fa-twitter fa-lg"></i></a></li>
+          <li class="google"><a href="https://plus.google.com/118033681540619304202/posts" title="Google+" target="_blank"><i class="fa fa-google-plus fa-lg"></i></a></li>
+          <li class="linkedin"><a href="http://www.linkedin.com/in/nickysnyds" title="LinkedIn" target="_blank"><i class="fa fa-linkedin fa-lg"></i></a></li>
+        </ul>
+      </div>
+          <!--<a href="https://www.facebook.com/nicholastsnyder" target="_blank">
+            <img src="http://icons.iconarchive.com/icons/lunartemplates/modern-social-media-rounded/48/Facebook-icon.png">
+          </a>
+          <a href="https://www.linkedin.com/profile/view?id=107384729&authType=NAME_SEARCH&authToken=-Krt&locale=en_US&srchid=1073847291413265423298&srchindex=1&srchtotal=150&trk=vsrp_people_res_name&trkInfo=VSRPsearchId%3A1073847291413265423298%2CVSRPtargetId%3A107384729%2CVSRPcmpt%3Aprimary" target="_blank"> 
+            <img src="http://icons.iconarchive.com/icons/lunartemplates/modern-social-media-rounded/48/LinkedIn-icon.png">
+          </a>
+          <a href="http://instagram.com/nickysnyds" target="_blank">
+            <img src="http://icons.iconarchive.com/icons/lunartemplates/modern-social-media-rounded/48/Instagram-icon.png">
+          </a>-->
+    </div>
+    <section class="footer-links"> 
+      <!-- FOOTER MENU -->
+      <ul class="footer-menu">
+        <li class="subfooter">
+          <a href="/experience" class="title">Experience</a>
+          <ul>
+            <li class="items"><a href="http://www.invoca.com" target="_blank">Invoca</a></li>
+            <li class="items"><a href="http://www.sproutup.org" target="_blank">Sprout Up</a></li>
+            <li class="items"><a href="http://gauchosplay.com/" target="_blank">UCSB Recreation Sports</a></li>
+          </ul>
+        </li> 
+        <li class="subfooter">
+          <a href="/education" class="title">Education</a>
+          <ul>
+            <li class="items"><a href="http://www.ucsb.edu/" target="_blank">University of California, Santa Barbara</a></li>
+            <li class="items"><a href="http://www.es.ucsb.edu/" target="_blank">Environmental Studies</a></li>
+            <li class="items"><a href="http://www.tmp.ucsb.edu/" target="_blank">Technology Management Program</a></li>
+          </ul>
+        </li>
+        <li class="subfooter">
+          <a href="/hobbies" class="title">Hobbies</a>
+          <ul></ul>
+        </li>
+        <li class="subfooter">
+          <a href="/references" class="title">References</a>
+          <ul></ul>
+        </li>
+        <li class="subfooter">
+          <a href="/contact" class="title">Contact</a>
+            <ul>
+              <li class="items"><a href="mailto:info@nickysnyds.com">Email</a></li>
+              <li class="items"><a href="tel:855-977-7567">Phone</a></li>
+            </ul>
+        </li>
+      </ul>
+      <!-- END/ FOOTER MENU --> 
+      <div class="footer-social">
+        <ul class="footer-social-nav">
+          <li class="facebook"><a href="https://www.facebook.com/nicholastsnyder" title="Facebook" target="_blank"><i class="fa fa-facebook fa-lg"></i></a></li>
+          <li class="twitter"><a href="https://twitter.com/nickysnyds" title="Twitter" target="_blank"><i class="fa fa-twitter fa-lg"></i></a></li>
+          <li class="google"><a href="https://plus.google.com/118033681540619304202/posts" title="Google+" target="_blank"><i class="fa fa-google-plus fa-lg"></i></a></li>
+          <li class="linkedin"><a href="http://www.linkedin.com/in/nickysnyds" title="LinkedIn" target="_blank"><i class="fa fa-linkedin fa-lg"></i></a></li>
+        </ul>
+      </div>
+    </section>
+  </body>
+</html>
